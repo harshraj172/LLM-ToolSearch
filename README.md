cd1650e64f6961ec2cb2df266d9ac18befa65085
Harsh Raj <harsh777111raj@gmail.com>
2023-07-18 12:50:32 +0530

    code abstraction & cleaning

--- README.md
+++ README.md
@@ -1,38 +1,20 @@
-# DSI-transformers
-A huggingface transformers implementation of [Transformer Memory as a Differentiable Search Index](https://arxiv.org/abs/2202.06991), Yi Tay, Vinh Q. Tran, Mostafa Dehghani, Jianmo Ni, Dara Bahri, Harsh Mehta, Zhen Qin, Kai Hui, Zhe Zhao, Jai Gupta, Tal Schuster, William W. Cohen, Donald Metzler
+# LLM Tool Search
 
-Requirements: `python=3.8` `transformers=4.17.0` `datasets=1.18.3` `wandb`
-> Note: This is not the official repository.
+## Files
+- `generate.py` samples output of a given input to produce variations using different temperatures and input paraphrases. It produces outputs using the default/normal sampling method and the ranking trick to be proposed.
+- `score.py` scores the outputs generated using the default/normal sampling method and the ranking trick to be proposed.
+- `score_utils` utility functions for `score.py`.
+- `example-consistency_paraphrasing.ipynb` proof of concept of the idea.
 
-## Updates
-- Check out our new repository for DSI training: https://github.com/ArvinZhuang/DSI-QG
-
-## Goal of this repository
-Reproduce the results of DSI Large, Naive String Docid, NQ10K. According to Table 3 in the original paper, we should have `Hits@1=0.347`,`Hits@10=0.605`
-
-### Step1: Create NQ10K training (indexing) and validation datasets
-
+## Setup
 ```
-cd data/NQ
-python3 create_NQ_train_vali.py
+conda create env python=3.9
+conda activate env
+pip install -r requirements.txt
 ```
-
-### Step2: Run training script
-cd back to the root directory and run:
-
-```
-python3 train.py
-```
-The training can be run with a single Tesla-v100 32G GPU. We use [wandb](https://wandb.ai/site) to log the Hits scores during training:
-
-![.im](hits_plots.png)
-
-
-### Discussion
-
-As the above plots show, the current implementation is worse than what is reported in the original paper, there are many possible reasons: the ratio of training and indexing examples (we use 1:1), number of training steps, the way of constructing documents text, etc. Although, seems the scores are on par with BM25 already.
-
-If you can identify the reason or any bug, welcome to open a PR to fix it!
-
-#### Indexing or overfitting?
-The training script also logged the hit@1 scores on the training set during training, this is aimed to analyze if the model can memorize all the training data points, the authors called this 'indexing' which I believe is just letting the model overfits the training data points. It turns out the model can reach %99.99 hit@1 on the training set very quickly (quickly overfit), but the hits scores on the test set continue going up. Seems T5 large has good generalizability on this task.
+## Experiments
+1. There are 4 varians of comparison:
+    a. Finetuning (input=query, output=function name) - `python train.py --finetune_type 1 --dataset_name API --model_name t5-base`
+    b. Finetuning with docID (input=query, output=docID) - `python train.py --finetune_type 2 --dataset_name API --model_name t5-base`
+    c. DSI (input=query, output=docID) - `python train.py --finetune_type 3 --dataset_name API --model_name t5-base`
+    d. DSI w/o docID (input=query, output=function name) - `python train.py --finetune_type 4 --dataset_name API --model_name t5-base`
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 from data import IndexingTrainDataset, IndexingCollator, QueryEvalCollator
 from transformers import T5Tokenizer, T5ForConditionalGeneration, TrainingArguments, TrainerCallback
 from trainer import IndexingTrainer
@@ -9,11 +10,12 @@ from tqdm import tqdm
 
 
 class QueryEvalCallback(TrainerCallback):
-    def __init__(self, test_dataset, logger, args: TrainingArguments, tokenizer: T5Tokenizer):
+    def __init__(self, test_dataset, logger, restrict_decode_vocab, args: TrainingArguments, tokenizer: T5Tokenizer):
         self.tokenizer = tokenizer
         self.logger = logger
         self.args = args
         self.test_dataset = test_dataset
+        self.restrict_decode_vocab = restrict_decode_vocab
         self.dataloader = DataLoader(
             test_dataset,
             batch_size=self.args.per_device_eval_batch_size,
@@ -33,16 +35,24 @@ class QueryEvalCallback(TrainerCallback):
         for batch in tqdm(self.dataloader, desc='Evaluating dev queries'):
             inputs, labels = batch
             with torch.no_grad():
-                batch_beams = model.generate(
-                    inputs['input_ids'].to(model.device),
-                    max_length=20,
-                    num_beams=10,
-                    num_return_sequences=10,
-                    early_stopping=True, ).reshape(inputs['input_ids'].shape[0], 10, -1)
+                if self.restrict_decode_vocab:
+                    batch_beams = model.generate(
+                        inputs['input_ids'].to(model.device),
+                        max_length=20,
+                        num_beams=10,
+                        prefix_allowed_tokens_fn=self.restrict_decode_vocab,
+                        num_return_sequences=10,
+                        early_stopping=True, ).reshape(inputs['input_ids'].shape[0], 10, -1)
+                else:
+                    batch_beams = model.generate(
+                        inputs['input_ids'].to(model.device),
+                        max_length=20,
+                        num_beams=10,
+                        num_return_sequences=10,
+                        early_stopping=True, ).reshape(inputs['input_ids'].shape[0], 10, -1)
                 for beams, label in zip(batch_beams, labels):
                     rank_list = self.tokenizer.batch_decode(beams,
                                                             skip_special_tokens=True)  # beam search should not return repeated docids but somehow due to T5 tokenizer there some repeats.
-                    print('np.array(rank_list)[:10] = ', np.array(rank_list)[:10])
                     hits = np.where(np.array(rank_list)[:10] == label)[0]
                     if len(hits) != 0:
                         hit_at_10 += 1
@@ -66,33 +76,53 @@ def compute_metrics(eval_preds):
 
 
 def main():
-    model_name = "t5-base"
     L = 50  # only use the first 32 tokens of documents (including title)
 
     # We use wandb to log Hits scores after each epoch. Note, this script does not save model checkpoints.
     wandb.login()
-    wandb.init(project="llm_tool_search", name='chatweb3-t5-base')
+    wandb.init(project="llm_tool_search", name=f'{args.dataset_name}-{args.model_name}')
 
-    tokenizer = T5Tokenizer.from_pretrained(model_name, cache_dir='cache')
-    model = T5ForConditionalGeneration.from_pretrained(model_name, cache_dir='cache')
+    tokenizer = T5Tokenizer.from_pretrained(args.model_name, cache_dir='cache')
+    model = T5ForConditionalGeneration.from_pretrained(args.model_name, cache_dir='cache')
 
-    train_dataset = IndexingTrainDataset(path_to_data='data/chatweb3/chatweb3_multi_task_train.json',
+    train_dataset = IndexingTrainDataset(path_to_data=f"data{'-finetune' if args.finetune_type in [1, 2] else ''}/{args.dataset_name}/{args.train_file_name}",
                                          max_length=L,
                                          cache_dir='cache',
-                                         tokenizer=tokenizer)
+                                         tokenizer=tokenizer,
+                                         finetune=(args.finetune_type in [1, 2]))
     
     # This eval set is really not the 'eval' set but used to report if the model can memorise (index) all training data points.
-    eval_dataset = IndexingTrainDataset(path_to_data='data/chatweb3/chatweb3_multi_task_train.json',
+    eval_dataset = IndexingTrainDataset(path_to_data=f"data{'-finetune' if args.finetune_type in [1, 2] else ''}/{args.dataset_name}/{args.val_file_name}",
                                         max_length=L,
                                         cache_dir='cache',
-                                        tokenizer=tokenizer)
+                                        tokenizer=tokenizer,
+                                        finetune=(args.finetune_type in [1, 2]))
     
     # This is the actual eval set.
-    test_dataset = IndexingTrainDataset(path_to_data='data/chatweb3/chatweb3_valid.json',
+    test_dataset = IndexingTrainDataset(path_to_data=f"data{'-finetune' if args.finetune_type in [1, 2] else ''}/{args.dataset_name}/{args.val_file_name}",
                                         max_length=L,
                                         cache_dir='cache',
-                                        tokenizer=tokenizer)
+                                        tokenizer=tokenizer,
+                                        finetune=(args.finetune_type in [1, 2]))
 
+    ################################################################
+    # docid generation constrain, we only generate integer docids.
+    SPIECE_UNDERLINE = "▁"
+    INT_TOKEN_IDS = []
+    for token, id in tokenizer.get_vocab().items():
+        if token[0] == SPIECE_UNDERLINE:
+            if token[1:].isdigit():
+                INT_TOKEN_IDS.append(id)
+        if token == SPIECE_UNDERLINE:
+            INT_TOKEN_IDS.append(id)
+        elif token.isdigit():
+            INT_TOKEN_IDS.append(id)
+    INT_TOKEN_IDS.append(tokenizer.eos_token_id)
+
+    def restrict_decode_vocab(batch_idx, prefix_beam):
+        return INT_TOKEN_IDS
+    ################################################################    
+    
     training_args = TrainingArguments(
         output_dir="./results",
         learning_rate=0.0005,
@@ -109,9 +139,15 @@ def main():
         save_strategy='no',
         # fp16=True,  # gives 0/nan loss at some point during training, seems this is a transformers bug.
         dataloader_num_workers=10,
-        gradient_accumulation_steps=2
+        gradient_accumulation_steps=args.gradient_accumulation_steps
     )
-
+    
+    if args.finetune_type in [2, 3]:
+        kwargs = \
+        {'callbacks': [QueryEvalCallback(test_dataset, wandb, restrict_decode_vocab, training_args, tokenizer)],
+         'restrict_decode_vocab': restrict_decode_vocab,}
+    else:
+        kwargs = {'callbacks': [QueryEvalCallback(test_dataset, wandb, None, training_args, tokenizer)],}
     trainer = IndexingTrainer(
         model=model,
         tokenizer=tokenizer,
@@ -123,11 +159,50 @@ def main():
             padding='longest',
         ),
         compute_metrics=compute_metrics,
-        callbacks=[QueryEvalCallback(test_dataset, wandb, training_args, tokenizer)],
+        **kwargs
     )
     trainer.train(
     )
 
 
 if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description='parser to run the script')
+
+    # add arguments
+    parser.add_argument('--finetune_type',
+                        type=int,
+                        default=1,
+                        help='''Method for finetuning:
+                        1 - finetuning (input=query, output=function name)
+                        2 - finetuning with docID (input=query, output=docID)
+                        3 - DSI (input=query, output=docID)
+                        4 - DSI w/o docID (input=query, output=function name)
+                        ''')
+    parser.add_argument('--dataset_name',
+                        type=str,
+                        default="API",
+                        help='name of the dataset')
+    parser.add_argument('--model_name',
+                        type=str,
+                        default="t5-base",
+                        help='model to be finetuned')
+    args = parser.parse_args()
+    if args.finetune_type == 1:
+        args.train_file_name = f'{args.dataset_name}_train.json'
+        args.val_file_name = f'{args.dataset_name}_valid'
+        args.gradient_accumulation_steps = 1
+    elif args.finetune_type == 2:
+        args.train_file_name = f'{args.dataset_name}_train-docid.json'
+        args.val_file_name = f'{args.dataset_name}_valid-docid.json'
+        args.gradient_accumulation_steps = 1
+    elif args.finetune_type == 3:
+        args.train_file_name = f'{args.dataset_name}_multi_task_train-docid.json'
+        args.val_file_name = f'{args.dataset_name}_valid-docid.json'
+        args.gradient_accumulation_steps = 2
+    elif args.finetune_type == 4:
+        args.train_file_name = f'{args.dataset_name}_multi_task_train.json'
+        args.val_file_name = f'{args.dataset_name}_valid.json'
+        args.gradient_accumulation_steps = 2
+        
     main()
